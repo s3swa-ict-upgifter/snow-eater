281f3810b273d5274b579f7b168f24379f9f89d1
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-09 17:56:33 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 var monster1 = new snowEaterProto(0,0); // Creats a new snowEater monster.
 var flake = "";	// A Flake object.
 var timer;	// Timer for running the flakes.
-var monsterSpeed = 20;	// Variable giving the speed to move the monster.
+var monsterSpeed = 40;	// Variable giving the speed to move the monster.
 var snowFallSpeed = 10; // Variable giving the speed to flakes to fall.
 var pointsbegin = 10;
 var endImage = [
