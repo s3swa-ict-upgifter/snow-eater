4bca1721ad1ee3d29e3e8307290ad28e6840728f
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-02-17 15:14:24 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ var flake = "";	// A Flake object.
 var timer;	// Timer for running the flakes.
 var monsterSpeed = 20;	// Variable giving the speed to move the monster.
 var snowFallSpeed = 15; // Variable giving the speed to flakes to fall.
+var pointsbegin = 10;
 
 var swallowImageSources =
   [
@@ -13,12 +14,12 @@ var swallowImageSources =
 
 // SnowEater object prototype:
 function snowEaterProto(x,y){
-	
+
   this.x = x;
   this.y = y;
   this.id = "snowEater";
   this.src= "images/snowEater.png";
-  this.move = function (step) {	
+  this.move = function (step) {
     if(step === undefined){
       step = monsterSpeed;
     }
@@ -30,8 +31,6 @@ function snowEaterProto(x,y){
   };
 }
 
-
-
 // SnowFlake object constructor:
 function snowFlake(x, y, id) {
   this.x = x;
@@ -39,77 +38,97 @@ function snowFlake(x, y, id) {
   this.id = id;
 }
 
+function swallow(monsterId){
+  var index = 0;
+  var src = "";
+  var timer2 = setInterval(showNextImage, 40);
+  function showNextImage(){
+    if(index < swallowImageSources.length){
+      src = swallowImageSources[index];
+      find(monsterId).src = src;
+      index++;
+    } else{
+      clearInterval(timer2);
+    }
+  }
+}
+
 // Creates a snowFlake and HTML-elements containing the flake image.
 function createFlake(x,y){
-	
+
   var i;
-	
+
   // Creates a new snowFlake object with location and id:
   flake=new snowFlake(x,y, "snowFlake"+1);
-	
+
   // Creates an img element for the flake and appends it to body:
-  var flakeElem = document.createElement("img");   
-	
+  var flakeElem = document.createElement("img");
+
   // Creates and sets an src attribute to the flakeElem:
-  var src = document.createAttribute("src");       
-  src.value = "images/snowFlake.png";                         
-  flakeElem.setAttributeNode(src);   
-	
+  var src = document.createAttribute("src");
+  src.value = "images/snowFlake.jpg";
+  flakeElem.setAttributeNode(src);
+
   // Creates and sets a class attribute value:
-  var class_attr = document.createAttribute("class");       
-  class_attr.value = "snowFlake";                         
-  flakeElem.setAttributeNode(class_attr); 
+  var class_attr = document.createAttribute("class");
+  class_attr.value = "snowFlake";
+  flakeElem.setAttributeNode(class_attr);
 
   // Creates and sets a id attribute value:
-  var id_attr = document.createAttribute("id");       
-  id_attr.value = flake.id;                         
-  flakeElem.setAttributeNode(id_attr); 
-	 
-  // Sets the position: 
+  var id_attr = document.createAttribute("id");
+  id_attr.value = flake.id;
+  flakeElem.setAttributeNode(id_attr);
+
+  // Sets the position:
   flakeElem.style.left = x+"px";
   flakeElem.style.top = y+"px";
-	  
-  document.body.appendChild(flakeElem); 
-	
+
+  document.body.appendChild(flakeElem);
+
 }
+
+function endGame(){
+  window.alert('you finised the game');
+
+}
+
 // Keeps the flakes falling and checks also if a flake
 // has been caught.
 function letItSnow(){
-	
+
   var i, x, y, flakeElem;
   var fallingStep = snowFallSpeed;
   flake.y += fallingStep;
-	
+
   var flakeElem = find(flake.id);
-	
+
   if(flakeElem){
     flakeElem.style.top = flake.y+"px";
-		
+
     // When the flake hits the groundLevel, it will be sent back up.
     var groundLevel = getWindowHeight()-100; // from the roof!
-		
+
     var monsterMouthLevel = groundLevel-70;
     var monsterMouthLeft = monster1.x+10;	// Left part of mouth.
     var monsterMouthRight = monster1.x+130;	// Right part of mouth.
-		
+
     // Random x value for the "new" flake:
-    var random_x = Math.floor((Math.random() * getWindowWidth()));  
-		
+    var random_x = Math.floor((Math.random() * getWindowWidth()));
+
     // Test first if flake is inside the mouth:
     if(flake.x > monsterMouthLeft &&
       flake.x < monsterMouthRight &&
       flake.y > monsterMouthLevel){
-      swallow(monster1)
-			
+
       sendFlakeUp(random_x, flakeElem);
-			
+      swallow(monster1.id);
       // Adds a point:
-      addaPoint();
-			
+
     } else if(flake.y > groundLevel){
       sendFlakeUp(random_x, flakeElem);
+      addaPoint();
     }
-		
+
     // Recursive call:
     timer = setTimeout("letItSnow()",40);
   }
@@ -149,25 +168,14 @@ function find(id){
 // Adds one to the points:
 var addaPoint = function (){
   var currentAmount = parseInt(find("amount").innerHTML);
-  var newAmount = currentAmount+1;
-  find("amount").innerHTML = newAmount; 
-};
-
-// Shows a series of images that make the monster swallow:
-function swallow(monsterId){
-  var index = 0;
-  var src = "";
-  var timer2 = setInterval(showNextImage, 40);
-  function showNextImage(){
-    if(index < swallowImageSources.length){
-      src = swallowImageSources[index];
-      find(monsterId).src = src;
-      index++;
-    } else{
-      clearInterval(timer2);
-    }
+  if (currentAmount == 0) {
+    endGame();
+    return window.location.reload();
+  } else {
+    var newAmount = currentAmount-1;
+    find("amount").innerHTML = newAmount;
   }
-}
+};
 
 // Sends the flake back up to the point (x,0):
 var sendFlakeUp = function (x, flakeElem){
@@ -185,7 +193,7 @@ function getWindowHeight(){
   var height = window.innerHeight
     || document.documentElement.clientHeight
     || document.body.clientHeight;
-	
+
   return height;
 }
 // Returns the width of the window:
@@ -196,4 +204,3 @@ function getWindowWidth(){
 
   return width;
 }
-
