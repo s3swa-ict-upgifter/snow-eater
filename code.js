7ec47661d0ed86edc8a4ef521bc084ad71a9e309
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-06-08 08:57:41 +0200

    Create code.js

--- code.js
+++ code.js
@@ -84,7 +84,7 @@ function createFlake(x,y){
   // Creates and sets an src attribute to the flakeElem:
 
   var src = document.createAttribute("src");
-  src.value = "images/galshir-spinner.gif";
+  src.value = "images/fidget.gif.gif";
   flakeElem.setAttributeNode(src);
 
   // Creates and sets a class attribute value:
